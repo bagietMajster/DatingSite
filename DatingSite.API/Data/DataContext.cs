d748724f71a12f2db79135be059d1ebaae036d5d
Kamil Lenart <kamil.lenart@op.pl>
2019-09-20 14:19:28 +0200

    Added DB

---
+++ DatingSite.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingSite.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingSite.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ValueModel> Values { get; set; }
+    }
+}
---
+++ DatingSite.API/Models/ValueModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingSite.API.Models
+{
+    public class ValueModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
