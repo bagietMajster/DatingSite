2618c1389ff1bafafa65d8233414ae5ed9e7a662
Kamil Lenart <kamil.lenart@op.pl>
2019-09-20 14:59:42 +0200

    Added Angular

--- DatingSite.API/Controllers/ValuesController.cs
+++ DatingSite.API/Controllers/ValuesController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingSite.API.Data;
+using DatingSite.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingSite.API.Controllers
 {
@@ -10,36 +13,56 @@ namespace DatingSite.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> AddValue([FromBody] ValueModel value)
         {
+            _context.Values.Add(value);
+            await _context.SaveChangesAsync();
+            return Ok(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> EditValue(int id, [FromBody] ValueModel value)
         {
+            var data = await _context.Values.FindAsync(id);
+            data.Name = value.Name;
+            _context.Values.Update(data);
+            await _context.SaveChangesAsync();
+            return Ok(data);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteValue(int id)
         {
+            var data = await _context.Values.FindAsync(id);
+            _context.Values.Remove(data);
+            await _context.SaveChangesAsync();
+            return Ok(data);
         }
     }
 }
