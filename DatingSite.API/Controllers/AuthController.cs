acd0dcd0f1be88e9ec7a8e354c026049db8fc95a
Kamil Lenart <kamil.lenart@op.pl>
2019-09-21 17:38:30 +0200

    Added Auth

---
+++ DatingSite.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using DatingSite.API.Data;
+using DatingSite.API.Dtos;
+using DatingSite.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingSite.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repository;
+
+        public AuthController(IAuthRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserFroRegisterDTO userFroRegisterDTO)
+        {
+            userFroRegisterDTO.Username = userFroRegisterDTO.Username.ToLower();
+
+            if(await _repository.UserExists(userFroRegisterDTO.Username))
+            {
+                return BadRequest("This username is taken");
+            }
+
+            var userToCreate = new UserModel
+            {
+                UserName = userFroRegisterDTO.Username
+            };
+
+            var createdUser = await _repository.Register(userToCreate, userFroRegisterDTO.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ DatingSite.API/Dtos/UserFroRegisterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingSite.API.Dtos
+{
+    public class UserFroRegisterDTO
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(12,MinimumLength =6, ErrorMessage = "Password must consist of 6-12 characters")]
+        public string Password { get; set; }
+    }
+}
