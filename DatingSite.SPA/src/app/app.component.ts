3f0bbd1e1e744dc9f418ab15cb38b2f196d3bbb1
Kamil Lenart <kamil.lenart@op.pl>
2019-09-22 18:00:54 +0200

    Fully working login

--- DatingSite.SPA/src/app/app.component.ts
+++ DatingSite.SPA/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './Services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingSite-SPA';
+export class AppComponent implements OnInit {
+
+  jwtHelper = new JwtHelperService();
+
+  constructor(private authServices: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authServices.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
