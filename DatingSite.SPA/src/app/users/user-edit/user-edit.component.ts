077758901b7c2dde7da8dd53e275bd79f6966786
Kamil Lenart <kamil.lenart@op.pl>
2019-09-24 19:54:56 +0200

    Added guard

--- DatingSite.SPA/src/app/users/user-edit/user-edit.component.ts
+++ DatingSite.SPA/src/app/users/user-edit/user-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserModel } from 'src/app/models/UserModel';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/Services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
@@ -10,8 +12,9 @@ import { ActivatedRoute } from '@angular/router';
 export class UserEditComponent implements OnInit {
 
   user: UserModel;
+  @ViewChild('editForm') editForm: NgForm;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -19,4 +22,8 @@ export class UserEditComponent implements OnInit {
     });
   }
 
+  updateUser() {
+    this.alertify.success('Profile saved');
+    this.editForm.reset(this.user);
+  }
 }
